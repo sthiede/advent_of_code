44cd424266c2d3a846cc3e13c05ef402af25debe
Stephanie Thiede <thiede@umich.edu>
2020-12-09 00:29:03 -0500

    solved part 2!

--- challenge2/challenge2_code.R
+++ challenge2/challenge2_code.R
@@ -1,10 +1,14 @@
 # Given a list of rules and passwords, how many are valid?
 
+
 # READ IN INPUT DATA 
 passwords = read.table('challenge2/input_data/input')
 
 # Part 1 ----
 # Password rule is that the password must contain the specified range of letter
+
+# Using stringr to solve this problems! 
+library(stringr)
 
 # CLEAN DATA 
 # renames columns
@@ -14,7 +18,6 @@ passwords$Letter = gsub(':', '', passwords$Letter)
 
 
 # COUNT LETTERS IN STRING USING TIDYVERSE! 
-library(tidyverse)
 meets_criteria = rep(NA, nrow(passwords))
 for (i in 1:nrow(passwords)){
   
@@ -33,3 +36,28 @@ for (i in 1:nrow(passwords)){
 
 
 sum(meets_criteria)
+
+
+# Part 2 ----
+# Password rule describes 2 positions in the password. Exactly one of those positions
+# must contain the letter specified 
+
+# Can use substr in base R to get the character value at specified string positions 
+meets_criteria = rep(NA, nrow(passwords))
+for (i in 1:nrow(passwords)){
+  
+  # Get positions 
+  pos = as.vector(as.numeric(str_split(passwords$Range[i], '-', simplify = TRUE)))
+  
+  # Get value at position 1 and position 2 
+  values = c(substr(passwords$Password[i], pos[1], pos[1]), substr(passwords$Password[i], pos[2], pos[2]))
+  
+  # Meets criteria if only 1 value is the specified letter   
+  meets_criteria[i] = sum(values == passwords$Letter[i]) == 1
+ 
+   
+}
+
+sum(meets_criteria)
+
+passwords[meets_criteria,]
